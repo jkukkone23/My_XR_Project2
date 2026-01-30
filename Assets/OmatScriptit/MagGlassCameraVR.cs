b122df4a195114d25f9fe4537e4ea5aed10cb209
jkukkone23 <jkukkone23@student.oulu.fi>
2026-01-30 16:23:31 +0200

    Suurennuslasi ohjeineen valmi task2 kimppuun

--- Assets/OmatScriptit/MagGlassCameraVR.cs
+++ Assets/OmatScriptit/MagGlassCameraVR.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class MagClassCameraVR : MonoBehaviour
 {
-    public Transform xrCamera;        // VR-lasit (HMD)
-    public Transform magnifierLens;   // Suurennuslasiobjektin linssi
-    public float zoomFactor = 4f;     // Zoom
-    public Vector3 manualOffset = Vector3.zero; // Pieni offset linssin eteen
+    public Transform xrCamera;        // XR headset camera
+    public Transform magnifierLens;   // Magnifying glass lens
+    public float zoomFactor = 4f;     // Zoomfactor 4 meaning 4 times magnified
+    public Vector3 manualOffset = Vector3.zero; // Small offset if needed, this works funny so maybe dont use it?
 
     private Camera magCamera;
 
@@ -13,7 +13,7 @@ public class MagClassCameraVR : MonoBehaviour
     {
         magCamera = GetComponent<Camera>();
         if (magCamera == null)
-            Debug.LogError("MagClassCameraVR: Kameraa ei löydy!");
+            Debug.LogError("MagClassCameraVR: Camera not found!");
     }
 
     void LateUpdate()
@@ -21,20 +21,18 @@ public class MagClassCameraVR : MonoBehaviour
         if (xrCamera == null || magnifierLens == null || magCamera == null)
             return;
 
-        //  Lasketaan vektori HMD:n ja linssin sijainnin välillä
+        //  Calculate vector between HMD ja lens 
         Vector3 direction = (xrCamera.position - magnifierLens.position).normalized;
 
-        //  Asetetaan kamera lasin eteen offsetilla
-        float distance = Vector3.Distance(xrCamera.position, magnifierLens.position);
+        //  Set camera at the position OFFSET optional
         transform.position = magnifierLens.position + manualOffset;
 
-        //  Magnify out of same line as XR camera
+        //  Magnify to the same line as XR camera
         Vector3 targetPoint = magnifierLens.position - direction;
         transform.rotation = Quaternion.LookRotation(targetPoint - transform.position,xrCamera.up);
 
-        //transform.LookAt(targetPoint);
 
-        //  Zoomataan FOV:lla
+        //  Zoom with field of view to given zoom factor
         magCamera.fieldOfView = Mathf.Clamp(60f / zoomFactor, 1f, 179f);
     }
 }
